9157449c13f8be1e84e1db1086a84ffcae30c6fd
bohurtadop <bohurtadop@unal.edu.co>
2023-03-05 16:22:54 -0500

    Create models

---
+++ sophos-backend/src/main/java/com/sophos/model/Order.java
@@ -0,0 +1,37 @@
+package com.sophos.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "orders")
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    @Column(name = "customer_id")
+    private Integer customerId;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(Integer customerId) {
+        this.customerId = customerId;
+    }
+
+}
